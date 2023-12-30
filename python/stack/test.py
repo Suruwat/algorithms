ba69e1f68c5d445a7115a5056c7893a54f386aa5
Bhuwan Pandit <bp7968h@gre.ac.uk>
2023-12-30 17:14:23 +0000

    feat: stack implementation

---
+++ python/stack/test.py
@@ -0,0 +1,11 @@
+from Stack import Stack
+
+testStack = Stack()
+
+testStack.push(1)
+testStack.push(2)
+testStack.push(3)
+
+print(testStack.peek())  # Outputs: 3
+print(testStack.pop())   # Outputs: 3
+print(testStack.size())  # Outputs: 2
